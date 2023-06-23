c1b774b388d4f4c1852d5c07d85ddafa15759812
mtorkington <matthew.torkington@checkmarx.com>
2023-06-23 14:50:20 +0200

    Adding Unicode

--- src/main/java/com/rest/controller/test/RestControllerTestApplication.java
+++ src/main/java/com/rest/controller/test/RestControllerTestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestControllerTestApplication {
-
+// coment here to test chars Å Ä Ö
 	public static void main(String[] args) {
 		SpringApplication.run(RestControllerTestApplication.class, args);
 	}
